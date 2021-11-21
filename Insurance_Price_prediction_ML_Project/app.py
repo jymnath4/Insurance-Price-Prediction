eb021afeb2bd7556389b9aa70ee3292d71d0dc56
jymnath4 <65106009+jymnath4@users.noreply.github.com>
2021-11-21 12:37:49 +0530

    Update app.py

--- Insurance_Price_prediction_ML_Project/app.py
+++ Insurance_Price_prediction_ML_Project/app.py
@@ -14,7 +14,7 @@ app= Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("templates/home.html")
+    return render_template("home.html")
 
 @app.route("/result",methods=['POST','GET'])
 def result():
